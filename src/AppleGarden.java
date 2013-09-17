5b0f1dbded603ddd849113161664ea635f8fffe5
Aleksei Zhakov <JustLexius@gmail.com>
2013-09-17 21:51:50 +0300

    blossom mechanism, seeds dropped
    blossom mechanism added, sum of seeds dropped within punch now shown in
    console messages.

--- src/AppleGarden.java
+++ src/AppleGarden.java
@@ -4,24 +4,29 @@ import java.io.InputStreamReader;
 
 
 public class AppleGarden {
+	private static AppleTree tree;
+	
 	public static void main(String[] args) throws IOException, InterruptedException {
 		int option = 1;
-		while (option == 1){
+		while (option > 0 && option < 4){
 			printMenu();
 			BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
 			option = Integer.parseInt(reader.readLine());
 			switch (option) {
 			case 1:
-				startPunching();
+				growTree();
 				break;
 			case 2:
+				bloom();
+				break;
+			case 3:
+				startPunching();
 				break;
 			default:
-				option = 3;
 				break;
 			}
 		}
-		if (option == 2){
+		if (option == 4){
 			System.out.print("exiting");
 		} else {
 			System.out.print("invalid input... exiting");
@@ -29,23 +34,53 @@ public class AppleGarden {
 	}
 	
 	private static void printMenu(){
-		System.out.print("1. Grow tree\n2. Exit\n");
+		System.out.println("1. Grow tree");
+		System.out.println("2. Tree blossoms");
+		System.out.println("3. Grow apples & start punching");
+		System.out.println("4. Exit");
+	}
+	
+	private static void growTree(){
+		tree = new AppleTree();
+		System.out.println("New tree grown!");
+	}
+	
+	private static void bloom(){
+		if (tree.getBlossoms() == 0){
+			tree.bloom();
+			System.out.println("Tree blooms! Number of blossoms: " + tree.getBlossoms());
+		} else {
+			System.out.println("Tree already blooms! Number of blossoms: " + tree.getBlossoms());
+		}
+		
 	}
 	
 	private static void startPunching() throws InterruptedException{
-		AppleTree tree = new AppleTree();
-		tree.grow();
-		printStats(tree);
-		while(tree.getApplesCount() > 0){
-			System.out.print("punch!\n");
-			int applesFell = tree.punch();
-			System.out.print("Apples fell: " + applesFell + "\n");
+		if (tree.getBlossoms() == 0){
+			System.out.println("Nothing to gather!");
+		} else {
+			tree.grow();
 			printStats(tree);
-			Thread.sleep(150);
+			while(tree.getApplesCount() > 0){
+				System.out.print("punch!\n");
+				int applesFell = 0;
+				int seedsFell = 0;
+				Apple[] dropped = tree.punch();
+				for (int i = 0; i < 6; i++){
+					if (dropped[i] != null){
+						applesFell++;
+						seedsFell += dropped[i].getSeeds();
+					}
+				}
+				System.out.println("Apples fell: " + applesFell);
+				System.out.println("Seeds fell: " + seedsFell);
+				printStats(tree);
+				Thread.sleep(150);
+			}
 		}
 	}
 	
 	private static void printStats(AppleTree tree){
-		System.out.print("Apples still on the tree: " + tree.getApplesCount() + "\n");
+		System.out.println("Apples still on the tree: " + tree.getApplesCount());
 	}
 }
--- src/AppleTree.java
+++ src/AppleTree.java
@@ -2,32 +2,50 @@ import java.util.Random;
 
 public class AppleTree {
 	private Apple[] apples;
-	private int applesCount;
+	private int applesCount = 0;
+	private int blossoms = 0;
 	
 	public int getApplesCount() {
 		return applesCount;
 	}
 	
-	public void grow(){
-		Random rand = new Random();
-		apples = new Apple[rand.nextInt(50)];
-		applesCount = apples.length;
-		for(int i = 0; i < applesCount; i++){
-			apples[i] = new Apple();
+	public int getBlossoms() {
+		return blossoms;
+	}
+
+
+
+	public void bloom(){
+		if (blossoms == 0){
+			Random rand = new Random();
+			blossoms = rand.nextInt(50);
 		}
 	}
 	
-	public int punch(){
+	public void grow(){
+		if (blossoms != 0){
+			applesCount = blossoms;
+			apples = new Apple[blossoms];
+			for(int i = 0; i < applesCount; i++){
+				apples[i] = new Apple();
+			}
+			blossoms = 0;
+		}
+	}
+	
+	public Apple[] punch(){
 		Random rand = new Random();
+		Apple dropped[] = new Apple[6];
 		int applesFell = 0;
 		for(int i = 0; i < 6; i++){
 			int position = rand.nextInt(apples.length);
 			if (apples[position] != null){
+				dropped[i] = apples[position];
 				apples[position] = null;
 				applesFell++;
 			}
 		}
 		applesCount -= applesFell;
-		return applesFell;
+		return dropped;
 	}
 }
